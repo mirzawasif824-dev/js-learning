2e6eb4e16ae442867306523a1c84eae115955df3
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-06-30 08:36:08 +0000

    container config done

---
+++ 01-basics/variable.js
@@ -0,0 +1,16 @@
+const account_id = 123033
+let accoutEmail = "mirzawasif@gmial.com"
+var account_password = "123033"
+account_city = "Faisalabad"
+let accountstate;
+
+// account_id = 12345 // not allowed
+
+
+accountEmail="wasif@gmail.com"
+account_password= "331230"
+account_city="Lahore"
+
+console.log(account_id);
+
+console.table([account_id, accoutEmail, account_password, account_city, accountstate])
