eb66c0867b650198b7884628a033580946dc1671
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-06-30 09:43:59 +0000

    datatypes

---
+++ 01-basics/datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alert("hello") // we are using node not browser js
+
+let name ="Mirza Wasif "// this is a strig datatype.
+let age = 20 // this is a number data type => this has a range and that range is 2 to power 53.
+let isloggedin = true //this is bolean data type to check use is logging in or not. 
+let state;
+
+//but if the nunmber us too big then we use bigitn in java script.
+
+console.log(typeof "Mirza Wasif")
