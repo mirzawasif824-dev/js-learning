3d514fb68f875333e29ec826278c383e3b35e7c2
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-01 08:54:30 +0000

    stack-heap

---
+++ 01-basics/Stack-Heap.js
@@ -0,0 +1,20 @@
+ let myYoutubename="Wasi.com"
+
+ let anothername = myYoutubename
+ anothername = "chaiorcode" 
+
+ console.log(myYoutubename);
+ console.log(anothername);
+ 
+
+ let userOne = {
+    email: "user@gmail.com"
+    
+ }
+
+ let userTwo = userOne
+userTwo.email = "user2@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
