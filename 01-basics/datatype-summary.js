215b55930aca18e4c8e3b9b72af19634bfef7827
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-01 07:33:05 +0000

    datatype-summary

---
+++ 01-basics/datatype-summary.js
@@ -0,0 +1,32 @@
+const score = 100
+const scorevalue =  100.3
+
+const loggedin = false
+const outsidetemp = null
+
+let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id == anotherid);
+
+const bignumber = 123456789
+console.log(bignumber);
+
+
+const names = ["wasif", "usman", "Ali"];
+let myObj = {
+      name: "Mirza",
+      age:  20,
+
+}
+
+
+const myFunction =function(){
+    console.log("Hello World")
+} 
+
+console.log(typeof bignumber);
+console.log(typeof names);
+console.log(typeof outsidetemp);
+
