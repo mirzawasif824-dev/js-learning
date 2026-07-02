123bd233748a1407a88404facf8d79ac206a8d79
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-02 08:46:43 +0000

    date-time

---
+++ 01-basics/date-time.js
@@ -0,0 +1,13 @@
+// Dates:-
+
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+
+console.log(typeof mydate);
+
+//let createmydate = new Date(2026, 0, 16)
+let createmydate = new Date(2026, 0, 16, 5, 3)
+console.log(createmydate.toDateString());
+
