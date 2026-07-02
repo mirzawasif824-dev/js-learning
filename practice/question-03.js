8f9fcf2bffcf9032939fd416bbb0dc112fb59a7f
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-02 14:15:44 +0000

    question-03

--- practice/question-03.js
+++ practice/question-03.js
@@ -1,2 +1,11 @@
-const even= num => num/2;
-console.log(10);
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Number is not Even";
+  }
+}
+
+// Let's test it:
+console.log(checkEvenOrOdd(50)); 
+console.log(checkEvenOrOdd(27));  
