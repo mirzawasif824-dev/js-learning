b49df9c2252f4ffd0f26ca99c1989fd09a0b1b2d
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-02 11:17:25 +0000

    question-2

--- practice/question-02.js
+++ practice/question-02.js
@@ -1,1 +1,33 @@
-console.log(3+2)
+const sum = (firstnumber, secnumber) =>{
+return firstnumber + secnumber
+}
+
+console.log(sum(5, 10));
+
+
+const sub = (firstnumber, secnumber) =>{
+return firstnumber - secnumber
+}
+
+console.log(sub(5, 10))
+
+
+const mul = (firstnumber, secnumber) =>{
+return firstnumber * secnumber
+}
+
+console.log(mul(5, 10))
+
+
+const devide = (firstnumber, secnumber) =>{
+return firstnumber / secnumber
+}
+
+console.log(devide(5, 10))
+
+
+const percentage = (firstnumber, secnumber) =>{
+return firstnumber % secnumber
+}
+
+console.log(percentage(5, 10))
