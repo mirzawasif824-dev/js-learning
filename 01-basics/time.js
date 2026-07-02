e58a6fa7e55f54a60733999473d39618233f89cd
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-02 09:26:02 +0000

    time

---
+++ 01-basics/time.js
@@ -0,0 +1,10 @@
+//Time 
+
+let mytimestammp = Date.now()
+// console.log(mytimestammp);
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newdate = new Date()
+console.log(newdate.getMonth());
+console.log(newdate.getDate());
