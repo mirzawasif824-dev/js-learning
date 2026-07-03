1bf768ff56befbe1615e1fe061db13d23d976ead
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-03 06:32:46 +0000

    question-06

---
+++ practice/question-06.js
@@ -0,0 +1,9 @@
+
+function getReversedArray(num1, num2, num3) {
+    return [num1, num2, num3].reverse();
+}
+
+
+const myArray = getReversedArray(3, 2, 1);
+
+console.log("This is the reversed Array:", myArray);
