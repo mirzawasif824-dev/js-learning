81c0825ee9efb23f9d6e2ee1f1990a280fcb5a89
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-09 11:43:38 +0000

    question-09

---
+++ practice/question-09.js
@@ -0,0 +1,13 @@
+function removeDuplicates(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
