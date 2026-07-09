2c798097310a232dafc063900584c8fc8b5ca143
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-09 11:43:24 +0000

    question-06

---
+++ practice/question-08.js
@@ -0,0 +1,16 @@
+function countVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+
+console.log(countVowels("Hello World")); 
+console.log(countVowels("JavaScript"));   
