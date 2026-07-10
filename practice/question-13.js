73db399de7b7e41bb6af7800c8417031aeb535c1
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-10 06:32:30 +0000

    question-13

---
+++ practice/question-13.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+  let sequence = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence;
+}
+
+
+console.log(fibonacci(7)); 
