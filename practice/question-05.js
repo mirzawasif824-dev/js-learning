dbced841b4676909027cba0366b823e3ea22bc92
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-10 06:02:59 +0000

    question-05

--- practice/question-05.js
+++ practice/question-05.js
@@ -1,3 +1,7 @@
-function celsiustofahrenhiet(){
-    
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
 }
+
+console.log("This is the value of celsius");
+
+console.log(celsiusToFahrenheit(25));
