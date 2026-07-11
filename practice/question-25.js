3bb2a415ec57e52439fde4a8f44110a51e4b2033
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-11 07:14:06 +0000

    question-25

--- practice/question-25.js
+++ practice/question-25.js
@@ -1,13 +1,13 @@
 function isSorted(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false; 
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
     }
-  }
-  return true; 
+    return true;
 }
 
 
-console.log(isSorted([1, 2, 3, 4, 5])); 
-console.log(isSorted([1, 3, 2, 4]));    
+console.log(isSorted([1, 2, 3, 4, 5]));
+console.log(isSorted([1, 3, 2, 4]));
 console.log(isSorted([5]));             
