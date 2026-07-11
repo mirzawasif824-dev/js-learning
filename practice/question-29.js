aeedff1fa08531e7d4e0a1ecb616f80b12177000
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-11 10:34:57 +0000

    question-29

---
+++ practice/question-29.js
@@ -0,0 +1,11 @@
+function calculateAverage(numbers) {
+  let sum = 0;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  
+  return sum / numbers.length;
+}
+
+console.log(calculateAverage([10, 20, 30])); 
