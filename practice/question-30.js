4b3e055e30dd5a7c6ec36531107f76e8db4f8f99
mirzawasif824-dev <mirzawasif824@gmail.com>
2026-07-11 09:15:07 +0000

    question-30

---
+++ practice/question-30.js
@@ -0,0 +1,22 @@
+function findMinMax(arr) {
+  if (arr.length === 0) {
+    return { min: undefined, max: undefined };
+  }
+
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return { min, max };
+}
+
+const numbers = [12, 45, 2, 78, 34, 9];
+console.log(findMinMax(numbers)); 
